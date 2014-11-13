e87a0fdc43c45b91316ef2b12f93eb599ae4d153
dstoianov <denys.stoianov@gmail.com>
2014-11-14 01:48:27 +0200

    add some more code for my experiments with AllureReports

---
+++ src/main/java/my/company/steps/OnFailure.java
@@ -0,0 +1,56 @@
+package my.company.steps;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+import ru.yandex.qatools.allure.annotations.Attachment;
+import ru.yandex.qatools.allure.annotations.Step;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Dmitry Baev charlie@yandex-team.ru
+ *         Date: 08.09.14
+ */
+public class OnFailure extends TestListenerAdapter {
+
+    @Step("Hi, I'm step in your testng listener")
+    @Override
+    public void onTestFailure(ITestResult tr) {
+        createAttachment();
+    }
+
+//    @Attachment("Hi, I'm attachment in your testng listener")
+//    public String createAttachment() {
+//        return "My own attachment body!";
+//    }
+
+    @Attachment(type = "image/png")
+    public byte[] createAttachment() {
+        return captureScreenShot();
+    }
+
+
+    private byte[] captureScreenShot() {
+        try {
+            BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            ImageIO.write(image, "png", baos);
+            baos.flush();
+            byte[] imageInByte = baos.toByteArray();
+            baos.close();
+            return imageInByte;
+        } catch (IOException | AWTException e) {
+            e.printStackTrace();
+        }
+        return "Attachment Content Empty".getBytes();
+    }
+
+}
--- src/test/java/com/test/FirstAllureTest.java
+++ src/test/java/com/test/FirstAllureTest.java
@@ -11,11 +11,14 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import ru.yandex.qatools.allure.annotations.*;
+import ru.yandex.qatools.allure.config.AllureConfig;
 import ru.yandex.qatools.allure.model.SeverityLevel;
 
 import java.io.File;
 import java.sql.Driver;
 import java.util.Properties;
+
+import static org.testng.AssertJUnit.fail;
 
 /**
  * Created by dstoianov on 2014-11-06, 6:20 PM
@@ -26,10 +29,10 @@ public class FirstAllureTest {
     @Parameter("My Param")
     private String myParameter;
 
-   private WebDriver driver;
+    private WebDriver driver;
 
     @BeforeClass
-    public void setUp(){
+    public void setUp() {
 
         String property = System.getProperty("phantomjs.binary.path");
         String property2 = System.getProperty("webdriver.chrome.driver");
@@ -39,10 +42,12 @@ public class FirstAllureTest {
     }
 
     @AfterClass
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
 
+    @Issue("JIRA-254")
+    @Description("some description for this test")
     @Severity(SeverityLevel.MINOR)
     @Test
     public void testName1() throws Exception {
@@ -50,23 +55,54 @@ public class FirstAllureTest {
         makeScreenshot();
     }
 
+
+    @Issues({@Issue("JIRA-1"), @Issue("JIRA-2")})
     @Severity(SeverityLevel.BLOCKER)
-    @Test
+    @Test(description = "some description from test")
     public void testName2() throws Exception {
         myParameter = "papa";
         myParameter = "mapa";
         myParameter = "mama";
         driver.get("http://google.com.ua");
-        makeScreenshot();
+
+        String chromeDdriver = System.getProperty("webdriver.chrome.driver");
+
+        String direc = System.getProperty("allure.results.directory");
+        String issue = System.getProperty("my.test.var");
+        System.out.println("my.test.var            : ----> " + issue);
+        System.out.println("allure.results.directory : ----> " + direc);
+
+        String property1 = System.getProperty("project.version");
+
+        File defaultResultsDirectory = AllureConfig.getDefaultResultsDirectory();
+        AllureConfig allureConfig = new AllureConfig();
+        String s = allureConfig.getTestSuiteFileSuffix();
     }
 
+
+    @Title("This is the Big Title for test")
     @Severity(SeverityLevel.CRITICAL)
     @Features("My Feature")
     @Stories({"Story1", "Story2"})
     @Test
     public void testName3() throws Exception {
         driver.get("http://google.co.uk");
-        makeScreenshot();
+
+    }
+
+    @Title("Test that should fail")
+    @Severity(SeverityLevel.TRIVIAL)
+    @Test
+    public void failedTest() {
+        fail();
+    }
+
+
+    @Test
+    public void testName4() throws Exception {
+        driver.get("http://google.co.uk");
+
+        fail();
     }
 
 
