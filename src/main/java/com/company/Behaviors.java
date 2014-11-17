9987429ae0bc77aafc8caa482a97c43c6a068fdd
Denys Stoianov <dstoianov@plslogistics.com>
2014-11-17 13:30:35 +0200

    learning Allure

---
+++ src/main/java/com/company/Behaviors.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * @author Boris Serdyuk just-boris@yandex-team.ru
+ *         Date: 12/9/13
+ */
+public class Behaviors {
+    public class SimpleStories {
+        public static final String title = "SimpleStories";
+        public static final String SUCCESS_STORY = "SuccessStory";
+        public static final String PARAMETRIZED_STORY = "ParametrizedStory";
+    }
+
+    public class Screenshot {
+        public static final String title = "Screenshot";
+        public static final String WEBDRIVER_SCREENSHOT = "WebdriverScreenshot";
+        public static final String OTHER_SCREENSHOT = "OtherScreenshot";
+    }
+}
---
+++ src/test/java/my/company/AlwaysPassingTest.java
@@ -0,0 +1,38 @@
+package my.company;
+
+import com.company.Behaviors;
+import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Features;
+import ru.yandex.qatools.allure.annotations.Stories;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Boris Serdyuk just-boris@yandex-team.ru
+ *         Date: 12/12/13
+ */
+@Features(Behaviors.SimpleStories.title)
+@Stories(Behaviors.SimpleStories.SUCCESS_STORY)
+public class AlwaysPassingTest {
+
+    @Test
+    public void testOne() throws Exception {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testTwo() throws Exception {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testThree() throws Exception {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testFour() throws Exception {
+        assertTrue(true);
+    }
+
+}
