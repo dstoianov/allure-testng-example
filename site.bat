37c44d4f26093c6d9650a8f39bde1a34797d3945
dstoianov <denys.stoianov@gmail.com>
2014-11-24 23:54:03 +0200

    add site and check_libs bat files

--- site.bat
+++ site.bat
@@ -1,1 +1,1 @@
-mvn -Dallure.issues.tracker.pattern=http://google.com/issues/%%s site
+mvn -Dallure.issues.tracker.pattern=http://google.com/issues/%%s -Dproject.build.sourceEncoding=UTF-8 -Dproject.reporting.outputEncoding=UTF-8 site
