c2f36ddd6f569048a74bd9b7e3fe6ccc6d792c81
dstoianov <denys.stoianov@gmail.com>
2016-04-21 00:49:19 +0200

    add data provider with changed name

---
+++ src/test/java/data/provider/TestDataProvider.java
@@ -0,0 +1,21 @@
+package data.provider;
+
+import org.testng.annotations.DataProvider;
+
+/**
+ * Created by Funker on 21.04.2016.
+ */
+public class TestDataProvider {
+
+    @DataProvider(name = "BasicDataProvider")
+    public Object[][] getTestData() {
+        Object[][] result = new Object[][]{
+                {new TestParameters("Germany", "Sample test 1")},
+                {new TestParameters("Austria", "Sample test 2")},
+                {new TestParameters("Norwegian", null)},
+                {new TestParameters("France", "Sample test 4")},
+                {new TestParameters("Netherlands", "Sample test 5")}
+        };
+        return result;
+    }
+}
---
+++ src/test/java/data/provider/TestParameters.java
@@ -0,0 +1,15 @@
+package data.provider;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Created by Funker on 20.04.2016.
+ */
+@Data
+@AllArgsConstructor
+public class TestParameters {
+
+    private String country;
+    private String data;
+}
