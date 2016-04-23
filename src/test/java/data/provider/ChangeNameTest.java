e84eed4b08f200c96a169862590f0139e6f6b7a5
dstoianov <denys.stoianov@gmail.com>
2016-04-23 18:24:27 +0200

    add test where method name of changing

--- src/test/java/data/provider/ChangeNameTest.java
+++ src/test/java/data/provider/ChangeNameTest.java
@@ -1,6 +1,7 @@
 package data.provider;
 
 import bean.TestParameters;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -9,40 +10,24 @@ import static org.hamcrest.Matchers.*;
 /**
  * Created by Funker on 20.04.2016.
  */
-public class ChangeNameTest /*implements ITest*/ {
+public class ChangeNameTest extends ChangeTestNameBaseTest {
 
-//    private String testName;
-//
-//    @Override
-//    public String getTestName() {
-//        return testName;
-//    }
-//
-//    @BeforeMethod(alwaysRun = true)
-//    public void testData(Method method, Object[] testData) {
-//        String testCase = "";
-//        if (testData != null && testData.length > 0) {
-//            TestParameters testParams = null;
-//            //Check if test method has actually received required parameters
-//            for (Object testParameter : testData) {
-//                if (testParameter instanceof TestParameters) {
-//                    testParams = (TestParameters) testParameter;
-//                    break;
-//                }
-//            }
-//            if (testParams != null) {
-//                testCase = testParams.getCountry();
-//            }
-//        }
-//        this.testName = String.format("%s(%s)", method.getName(), testCase);
-//    }
+    @DataProvider(name = "basicData")
+    public static Object[][] getTestData() {
+        Object[][] result = new Object[][]{
+                {new TestParameters("Germany", "Sample test 1")},
+                {new TestParameters("Austria", "Sample test 2")},
+                {new TestParameters("Norwegian", null)},
+                {new TestParameters("France", "Sample test 4")},
+                {new TestParameters("Netherlands", "Sample test 5")}
+        };
+        return result;
+    }
 
-
-    @Test(groups = {"change_name"}, dataProvider = "basicData", dataProviderClass = TestDataProvider.class)
+    @Test(groups = {"change_name"}, dataProvider = "basicData")
     public void testSample1(TestParameters data) {
         assertThat(data.getCountry(), is(not(nullValue())));
         assertThat(data.getData(), is(not(nullValue())));
     }
-
 
 }
---
+++ src/test/java/data/provider/ChangeTestNameBaseTest.java
@@ -0,0 +1,32 @@
+package data.provider;
+
+import bean.TestParameters;
+import org.testng.ITest;
+import org.testng.annotations.BeforeMethod;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by Funker on 21.04.2016.
+ */
+public class ChangeTestNameBaseTest implements ITest {
+
+    protected String testName;
+
+    @Override
+    public String getTestName() {
+        return testName;
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void initPages(Method method, Object[] parameters) {
+        testName = method.getName();
+        if (parameters.length > 0) {
+            for (Object parameter : parameters) {
+                if (parameter instanceof TestParameters) {
+                    testName = String.format("%s_%s", method.getName(), ((TestParameters) parameter).getCountry());
+                }
+            }
+        }
+    }
+}
